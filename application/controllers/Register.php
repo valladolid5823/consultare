76697325ef83b8f2dc088285344469594abbf847
valladolid5823 <valladolid5823@gmail.com>
2024-05-17 15:06:57 +0800

    Update files

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -15,11 +15,7 @@ class Register extends CI_Controller {
 		$this->load->view("template/footer");
     }
 
-    public function register_user() {
-
-		var_dump($_POST);
-
-		return;
+	public function register_user() {
         $this->form_validation->set_rules('username', 'Username', 'required');
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'required');
@@ -27,14 +23,26 @@ class Register extends CI_Controller {
         if ($this->form_validation->run() == FALSE) {
             $response = array('status' => 'error', 'message' => validation_errors());
         } else {
-            $data = array(
-                'username' => $this->input->post('username'),
-                'email' => $this->input->post('email'),
-                'password' => password_hash($this->input->post('password'), PASSWORD_BCRYPT)
-            );
+            $config['upload_path'] = './uploads/';
+            $config['allowed_types'] = 'gif|jpg|png';
+            $config['max_size'] = 2048; // 2MB
 
-            $this->db->insert('users', $data);
-            $response = array('status' => 'success', 'message' => 'User registered successfully!');
+            $this->upload->initialize($config);
+
+            if (!$this->upload->do_upload('userfile')) {
+                $response = array('status' => 'error', 'message' => $this->upload->display_errors());
+            } else {
+                $uploadData = $this->upload->data();
+                $data = array(
+                    'username' => $this->input->post('username'),
+                    'email' => $this->input->post('email'),
+                    'password' => password_hash($this->input->post('password'), PASSWORD_BCRYPT),
+                    'profile_picture' => $uploadData['file_name']
+                );
+
+                $this->db->insert('users', $data);
+                $response = array('status' => 'success', 'message' => 'User registered successfully!');
+            }
         }
 
         echo json_encode($response);
--- application/views/consultare/register.php
+++ application/views/consultare/register.php
@@ -1,38 +1,42 @@
 
-    <h2>Register</h2>
-    <form id="registerForm">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email"><br><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password"><br><br>
-		<label for="password">Upload:</label>
-        <input type="file" id="file" name="file_upload" required><br><br>
-        <button type="submit">Register</button>
-    </form>
-    <div id="response"></div>
+<h2>Register</h2>
+<form id="registerForm" enctype="multipart/form-data">
+	<label for="username">Username:</label>
+	<input type="text" id="username" name="username"><br><br>
+	<label for="email">Email:</label>
+	<input type="email" id="email" name="email"><br><br>
+	<label for="password">Password:</label>
+	<input type="password" id="password" name="password"><br><br>
+	<label for="userfile">Profile Picture:</label>
+	<input type="file" id="userfile" name="userfile"><br><br>
+	<button type="submit">Register</button>
+</form>
+<div id="response"></div>
 
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#registerForm').on('submit', function(e) {
-                e.preventDefault();
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+	$(document).ready(function() {
+		$('#registerForm').on('submit', function(e) {
+			e.preventDefault();
 
-                $.ajax({
-                    url: "<?php echo site_url('Register/register_user'); ?>",
-                    type: "POST",
-                    data: $(this).serialize(),
-                    dataType: "json",
-                    success: function(data) {
-                        if(data.status == 'error') {
-                            $('#response').html('<div style="color:red;">' + data.message + '</div>');
-                        } else {
-                            $('#response').html('<div style="color:green;">' + data.message + '</div>');
-                        }
-                    }
-                });
-            });
-        });
-    </script>
+			var formData = new FormData(this);
+
+			$.ajax({
+				url: "<?php echo site_url('Register/register_user'); ?>",
+				type: "POST",
+				data: formData,
+				contentType: false,
+				processData: false,
+				dataType: "json",
+				success: function(data) {
+					if(data.status == 'error') {
+						$('#response').html('<div style="color:red;">' + data.message + '</div>');
+					} else {
+						$('#response').html('<div style="color:green;">' + data.message + '</div>');
+					}
+				}
+			});
+		});
+	});
+</script>
 
